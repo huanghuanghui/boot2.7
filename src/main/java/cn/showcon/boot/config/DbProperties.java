ba3b62a80fe05c1d609998f357ab32808b9e7d8f
黄煌辉 <18065220802@163.com>
2022-07-26 16:45:55 +0800

    init2

--- src/main/java/cn/showcon/boot/config/DbProperties.java
+++ src/main/java/cn/showcon/boot/config/DbProperties.java
@@ -1,6 +1,7 @@
 package cn.showcon.boot.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "db")
 @RefreshScope
 @Getter
+@Setter
 public class DbProperties {
 
     private String name;
--- src/main/java/cn/showcon/boot/web/ContractBusinessController.java
+++ src/main/java/cn/showcon/boot/web/ContractBusinessController.java
@@ -1,15 +1,15 @@
 package cn.showcon.boot.web;
 
 import cn.showcon.boot.config.DbProperties;
-import cn.showcon.boot.config.PageRequest;
-import cn.showcon.boot.contract.ContractDetailResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -20,16 +20,29 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-05-04
  */
 @RestController
-@RequestMapping("/api/v1/contract/sign")
-@Tag(name = "合同中心相关服务")
 public class ContractBusinessController {
 
 
     @Autowired
     private DbProperties dbProperties;
-    @Operation(summary = "查询合同快照信息2")
-    @GetMapping("/snapshot2")
-    public String getContractSnapshot(PageRequest pageRequest) {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+    @Autowired
+    private BeanFactory beanFactory;
+    @Autowired
+    @Qualifier("systemProperties")
+    private Map<String, Object> systemProperties;
+    @Autowired
+    @Qualifier("environment")
+    private Environment environment;
+    @Autowired
+    @Qualifier("systemEnvironment")
+    private Map<String, Object> systemEnvironment;
+
+    @GetMapping("/get")
+    public String getContractSnapshot() {
+
         return dbProperties.getName();
     }
 
