a138a3c05ad2de1ae7a29b22f08a702ba0b1b50f
黄煌辉 <18065220802@163.com>
2022-11-28 13:56:59 +0800

    add final name

---
+++ src/main/java/cn/showcon/boot/User.java
@@ -0,0 +1,22 @@
+package cn.showcon.boot;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * <p>
+ * 描述:
+ * </p>
+ *
+ * @author hhh
+ * @since 2022/11/25
+ */
+@Getter
+@Setter
+@ToString
+public class User {
+
+    private String name;
+    private Integer age;
+}
--- src/main/java/cn/showcon/boot/controller/TestController.java
+++ src/main/java/cn/showcon/boot/controller/TestController.java
@@ -1,6 +1,10 @@
 package cn.showcon.boot.controller;
 
+import cn.showcon.boot.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,8 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    public String test(){
-        return "TEST_SUCCESS";
+    @GetMapping("/boot/test/{id}")
+    public Integer test(@PathVariable Integer id){
+        return id;
+    }
+
+    @PostMapping("/boot/post")
+    public String test(@RequestBody User user){
+        return user.toString();
     }
 }
