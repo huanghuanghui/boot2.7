db03a88daa8c08447126c31e0197beba22cd83ce
黄煌辉 <18065220802@163.com>
2023-01-30 16:30:18 +0800

    init

--- src/main/java/cn/showcon/boot/Application.java
+++ src/main/java/cn/showcon/boot/Application.java
@@ -1,14 +1,18 @@
 package cn.showcon.boot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class Application {
+
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  系统模块启动成功   ლ(´ڡ`ლ)ﾞ  ");
     }
-
-
 }
